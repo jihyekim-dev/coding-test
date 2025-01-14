f573a9f0ddb8b2da2d6e3d737edf170c575715d9
jihyekim-dev <kjihye06@naver.com>
2025-01-14 23:55:07 +0900

    add: 1 solution for Programmers Lv.1

--- Programmers/Lv.1/신고 결과 받기.js
+++ Programmers/Lv.1/신고 결과 받기.js
@@ -1,13 +1,23 @@
-function solution(left, right) {
-  // 정수 left ~ right까지 모든 수 중에서 약수 개수가 짝수인 수는 더하고,
-  // 약수의 개수가 홀수인 수들은 뺀 값을 return
-  let result = 0;
-  for (let i = left; i <= right; i++) {
-    let count = 0;
-    for (let j = 1; j <= i; j++) {
-      if (!(i % j)) count++;
+function solution(id_list, report, k) {
+  // id_list: 이용자 ID arr
+  // report: 각 이용자가 신고한 이용자 ID 정보 arr ('신고자ID 피신고자ID' 형태)
+  // k: 게시판 이용 정지 기준이 되는 신고 횟수
+  // 각 유저별로 정지 처리 결과 메일 받은 횟수 담은 배열 return
+  const processed = new Map();
+  for (let i = 0; i < report.length; i++) {
+    const [user, reported] = report[i].split(' ');
+    if (!processed.has(reported)) {
+      processed.set(reported, new Set());
+      processed.get(reported).add(user);
     }
-    result += !(count % 2) ? i : -i;
+    processed.get(reported).add(user);
   }
+
+  const result = Array(id_list.length).fill(0);
+  for (const [reported, users] of processed) {
+    if (users.size < k) continue;
+    users.forEach((v) => result[id_list.indexOf(v)]++);
+  }
+
   return result;
 }
